1ca343bf6f7552f8b2a096d11d9b33c858608bad
Harold Waters <waters.harold@gmail.com>
2020-03-16 20:17:25 -0500

    MEME-0005: Add sPoNgEbOb

---
+++ src/commands/sPoNgEbOb.ts
@@ -0,0 +1,48 @@
+import * as Discord from 'discord.js'
+import * as Canvas from 'canvas'
+import { Command } from '../interfaces'
+import { odds } from '../lib/util'
+
+export const sPoNgEbOb: Command = {
+  name: 'a sPonGe bOb mEmE',
+  description: 'I don\'t want to do the capitalization thing anymore',
+  trigger: (message: Discord.Message): boolean => {
+    if (odds(1, 100)) return false
+    if (message.cleanContent.toLowerCase().length < 32) return true
+    return false
+  },
+  execute: async (message: Discord.Message, args): Promise<void> => {
+    const mEsSaGge: string[] = []
+    for (let i = 0; i + 1 <= message.cleanContent.length; i += 2) {
+      mEsSaGge.push(message.cleanContent[i].toLowerCase())
+
+      if (message.cleanContent[i + 1] == null) continue
+
+      mEsSaGge.push(message.cleanContent[i + 1].toUpperCase())
+    }
+
+    const canvas = Canvas.createCanvas(680, 510)
+    const ctx = canvas.getContext('2d')
+
+    const background = await Canvas.loadImage('images/sPoNgEbOb.jpeg')
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
+
+    ctx.font = '72px impact'
+    ctx.fillStyle = '#ffffff'
+    ctx.strokeStyle = '#000000'
+
+    // Actually fill the text with a solid color
+    const x = 340
+    const y = 475
+    const maxWidth = 650
+    ctx.textAlign = 'center'
+
+    ctx.fillText(mEsSaGge.join(''), x, y, maxWidth)
+    ctx.strokeText(mEsSaGge.join(''), x, y, maxWidth)
+
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'sPoNgEbOb.jpg')
+    await message.channel.send(`<@${message.author.id}>`, attachment)
+  }
+}
+
+module.exports = sPoNgEbOb
