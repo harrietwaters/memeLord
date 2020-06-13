624f3a7dd78d0f1dfb383d358fca67f61fb28dc3
Harold Waters <waters.harold@gmail.com>
2020-06-12 22:23:51 -0500

    MEME-3014: Make TypeORM work if it kills me

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getConnectionOptions } from 'typeorm';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
     console.log('READY TO MEME!')
+    console.dir(getConnectionOptions, { depth: 10});
 }
 bootstrap();
