99ac748324fee0346b24f238ddd269ff24246d75
Harold Waters <waters.harold@gmail.com>
2020-06-12 22:12:36 -0500

    MEME-3014: Make TypeORM work if it kills me

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,6 @@ import { DiscordModule } from './discord/discord.module';
 import { CommandsModule } from './commands/commands.module';
 import { TriggeredEventsModule } from './triggered-events/triggered-events.module';
 import { UtilitiesModule } from './utilities/utilities.module';
-import { MemeHouse } from './models/meme-house.entity';
-import { ShitPost } from './models/shit-post.entity';
-import { SortingHatUser } from './models/sorting-hat-user.entity';
-import { Point } from './models/point.entity';
 import { HealthController } from './health/health.controller';
 
 @Module({
@@ -21,9 +17,7 @@ import { HealthController } from './health/health.controller';
         CommandsModule,
         TriggeredEventsModule,
         UtilitiesModule,
-        TypeOrmModule.forRoot({
-            entities: [ShitPost, MemeHouse, Point, SortingHatUser]
-        }),
+        TypeOrmModule.forRoot(),
         TerminusModule,
     ],
     controllers: [AppController, HealthController],
